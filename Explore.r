0b2c22c220dedde2bb3242af9cf8e48e52c7c69a
David Sheehy <sheehy_d@ymail.com>
2020-03-23 15:21:58 +0000

    some basic stuff

--- Explore.r
+++ Explore.r
@@ -4,3 +4,13 @@
 testdata = read.csv("train.csv", header = T)
 traindata = read.csv("test.csv", header = T)
 sampleSubmission = read.csv("sample_submission.csv", header = T)
+
+summary(testdata)
+
+## MSSubClass: Identiifies the type of dwelling involved in sale
+testdata$MSSubClass = as.factor(testdata$MSSubClass)
+traindata$MSSubClass = as.factor(testdata$MSSubClass)
+
+#### Need to you assign values to factor numbers, see data_description
+
+
