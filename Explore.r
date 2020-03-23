65ab9d965c0c65726f77143664072895e6d545c3
David Sheehy <sheehy_d@ymail.com>
2020-03-23 15:44:28 +0000

    examing sales prices distribution

--- Explore.r
+++ Explore.r
@@ -1,8 +1,9 @@
+install.packages("ggplot2")
+library(ggplot2)
 
 
-
-testdata = read.csv("train.csv", header = T)
-traindata = read.csv("test.csv", header = T)
+testdata = read.csv("test.csv", header = T)
+traindata = read.csv("train.csv", header = T)
 sampleSubmission = read.csv("sample_submission.csv", header = T)
 
 summary(testdata)
@@ -14,3 +15,23 @@ traindata$MSSubClass = as.factor(testdata$MSSubClass)
 #### Need to you assign values to factor numbers, see data_description
 
 
+### Some exploratory data analysis
+
+dim(traindata)
+
+### remove ID of houses and store for submission later
+Test_IDs = testdata$Id
+testdata$Id = NULL
+traindata$Id = NULL
+
+testdata$SalePrice = NA
+all =  rbind(traindata, testdata)
+
+dim(all)
+
+################ Examing the Distrubution of Sales Data
+
+ggplot(data = all[!is.na(all$SalePrice),],
+       aes(x=SalePrice)) +
+  geom_histogram(fill="blue", binwidth = 10000) +
+  scale_x_continuous(breaks = seq(0, 800000, by=100000))
