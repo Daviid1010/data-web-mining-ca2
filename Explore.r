3e3f51723cbb9f93873bfa29c607c42b36a77c5f
David Sheehy <sheehy_d@ymail.com>
2020-03-24 19:43:36 +0000

    basement NAs dealt with

--- Explore.r
+++ Explore.r
@@ -1,16 +1,16 @@
-install.packages("ggplot2")
-install.packages("knitr")
-install.packages("plyr")
-install.packages("dplyr")
-install.packages("corrplot")
-install.packages("caret")
-install.packages("gridExtra")
-install.packages("scales")
-install.packages("Rmisc")
-install.packages("ggrepel")
-install.packages("randomForest")
-install.packages("psych")
-install.packages("xgboost")
+##install.packages("ggplot2")
+##install.packages("knitr")
+##install.packages("plyr")
+##install.packages("dplyr")
+##install.packages("corrplot")
+##install.packages("caret")
+##install.packages("gridExtra")
+##install.packages("scales")
+##install.packages("Rmisc")
+##install.packages("ggrepel")
+##install.packages("randomForest")
+##install.packages("psych")
+##install.packages("xgboost")
 library(knitr)
 library(ggplot2)
 library(plyr)
@@ -193,3 +193,83 @@ all$GarageCond = as.character(all$GarageQual)
 all$GarageCond[is.na(all$GarageCond)] <- 'None'
 all$GarageCond = as.integer(revalue(all$GarageCond, Qualities))
 table(all$GarageCond)
+
+############# Basement Values
+summary(all$BsmtCond) ## 82 NA
+summary(all$BsmtExposure) ## 82 NA
+summary(all$BsmtFinSF1) # 1 NA
+summary(all$BsmtFinType1) # 79 NA
+summary(all$BsmtFinSF2) ### 1 NA
+summary(all$BsmtFinType2) ## 80 NA
+summary(all$BsmtFullBath) ## 2 NA
+summary(all$BsmtHalfBath) ## 2 NA
+summary(all$BsmtQual) ## 81 NA
+summary(all$BsmtUnfSF) ## 1 NA
+summary(all$TotalBsmtSF) ## 1 NA
+
+### BsmtFinType1 has 79 NAs, lets look at this
+all[!is.na(all$BsmtFinType1) & 
+      (is.na(all$BsmtCond)|
+         is.na(all$BsmtQual)|
+         is.na(all$BsmtExposure)|
+         is.na(all$BsmtFinType2)),
+    c('BsmtFinType1','BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType2')]
+
+## Based on these rows, it seems that 79 house have no basement,
+## all the other rows with NAs are mostly complete, we can impute values here
+### using most common variable values
+all$BsmtFinType2[333] <- names(sort(-table(all$BsmtFinType2)))[1]
+all$BsmtExposure[c(949, 1488, 2349)] <- names(sort(-table(all$BsmtExposure)))[1]
+all$BsmtCond[c(2041, 2186, 2525)] <- names(sort(-table(all$BsmtCond)))[1]
+all$BsmtQual[c(2218, 2219)] <- names(sort(-table(all$BsmtQual)))[1]
+
+## Basement Quality is Ordinal value
+all$BsmtQual = as.character(all$BsmtQual)
+all$BsmtQual[is.na(all$BsmtQual)] = 'None'
+all$BsmtQual = as.integer(revalue(all$BsmtQual, Qualities))
+table(all$BsmtQual)
+
+## Basement Condition is Ordinal
+all$BsmtCond = as.character(all$BsmtQual)
+all$BsmtCond[is.na(all$BsmtCond)] = 'None'
+table(all$BsmtCond)
+all$BsmtCond<-as.integer(revalue(all$BsmtCond, Qualities))
+table(all$BsmtCond)
+
+### Basement Exposore Ordinal
+table(all$BsmtExposure)
+all$BsmtExposure = as.character(all$BsmtExposure)
+all$BsmtExposure[is.na(all$BsmtExposure)] <- 'None'
+Exposure <- c('None'=0, 'No'=1, 'Mn'=2, 'Av'=3, 'Gd'=4)
+all$BsmtExposure<-as.integer(revalue(all$BsmtExposure, Exposure))
+table(all$BsmtExposure)
+
+## Basement Finish Type 1 is Ordinal, fin type values from data desccription
+all$BsmtFinType1 = as.character(all$BsmtFinType1)
+all$BsmtFinType1[is.na(all$BsmtFinType1)] <- 'None'
+FinType <- c('None'=0, 'Unf'=1, 'LwQ'=2, 'Rec'=3, 'BLQ'=4, 'ALQ'=5, 'GLQ'=6)
+
+all$BsmtFinType1<-as.integer(revalue(all$BsmtFinType1, FinType))
+table(all$BsmtFinType1)
+
+### Basement Finished Type 2 Oridinal, living conditions from data description
+all$BsmtFinType2 = as.character(all$BsmtFinType2)
+all$BsmtFinType2[is.na(all$BsmtFinType2)] <- 'None'
+FinType <- c('None'=0, 'Unf'=1, 'LwQ'=2, 'Rec'=3, 'BLQ'=4, 'ALQ'=5, 'GLQ'=6)
+
+all$BsmtFinType2<-as.integer(revalue(all$BsmtFinType2, FinType))
+table(all$BsmtFinType2)
+
+
+#### Now lets look at whats left in the NAs
+all[(is.na(all$BsmtFullBath)|is.na(all$BsmtHalfBath)|is.na(all$BsmtFinSF1)|is.na(all$BsmtFinSF2)|is.na(all$BsmtUnfSF)|is.na(all$TotalBsmtSF)), c('BsmtQual', 'BsmtFullBath', 'BsmtHalfBath', 'BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF')]
+
+##### these dont have basements and we will impute them as 0
+all$BsmtFullBath[is.na(all$BsmtFullBath)] <-0
+table(all$BsmtFullBath)
+all$BsmtHalfBath[is.na(all$BsmtHalfBath)] <-0
+table(all$BsmtHalfBath)
+all$BsmtFinSF1[is.na(all$BsmtFinSF1)] <-0
+all$BsmtFinSF2[is.na(all$BsmtFinSF2)] <-0
+all$BsmtUnfSF[is.na(all$BsmtUnfSF)] <-0
+all$TotalBsmtSF[is.na(all$TotalBsmtSF)] <-0
